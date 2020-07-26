ddb1ee1def8e88a22fe3e1a2c54632758fcf47e0
qinghua <qinghua.liu@thoughtworks.com>
2020-07-26 16:40:46 +0800

    create view activity and submit activity

--- app/src/main/java/com/thoughtworks/roompractice/MainActivity.java
+++ app/src/main/java/com/thoughtworks/roompractice/MainActivity.java
@@ -1,8 +1,12 @@
 package com.thoughtworks.roompractice;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.thoughtworks.roompractice.activities.SubmitActivity;
+import com.thoughtworks.roompractice.activities.ViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.submit_activity).setOnClickListener(view -> createSubmitActivity());
+
+        findViewById(R.id.view_activity).setOnClickListener(view -> createViewActivity());
+    }
+
+    private void createViewActivity() {
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
+    }
+
+    private void createSubmitActivity() {
+        Intent intent = new Intent(this, SubmitActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/thoughtworks/roompractice/activities/SubmitActivity.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.roompractice.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thoughtworks.roompractice.R;
+
+public class SubmitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit);
+    }
+}
